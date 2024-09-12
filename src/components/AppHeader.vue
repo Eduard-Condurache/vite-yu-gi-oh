a0565f3f8f8aee2f4f70ef39f6f0855228e9b6e2
Eduard-Condurache <eduardcondurache96@gmail.com>
2024-09-12 18:18:19 +0200

    Initial commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  data() {
+    return { 
+      message: 'Template Vite + Vue'
+    }
+  }
+}
+</script>
+
+<template>
+  <header>
+    <h1>
+      {{ message }}
+    </h1>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
